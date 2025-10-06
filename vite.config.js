29f368a99e125be52953c93d1e073421409b46a5
NaelHadrachi <nael.hadrachi-darricarrere@etu.u-pec.fr>
2025-10-06 13:09:17 +0200

    pckage vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Configuration Vite pour React + déploiement Vercel
 export default defineConfig({
   plugins: [react()],
+  base: '/', // important pour les routes absolues
+  build: {
+    outDir: 'dist', // dossier de sortie par défaut pour Vercel
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 })
